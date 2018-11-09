3c7073b06cc69e458cbbb331611666dbe67c2993
Eddie Cao <cao.yiqun@gmail.com>
2018-11-08 22:23:05 -0800

    restart

--- fish/config.fish
+++
@@ -1,5 +0,0 @@
-set -gx WORKON_HOME ~/devel/_envs/
-. ~/.config/fish/workon_funcs.fish
-. ~/.config/fish/keybinding.fish
-. ~/.config/fish/prompt.fish
-. ~/.config/fish/pip.fish
--- fish/pip.fish
+++
@@ -1,1 +0,0 @@
-set -gx PIP_DOWNLOAD_CACHE $HOME/devel/_envs/cache
