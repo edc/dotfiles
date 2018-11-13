7ed2f18137da145aae148185212004b6d9a10cba
eddie <edc@users.noreply.github.com>
2018-11-12 19:13:18 -0800

    prompt with OS hint

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,11 +2,17 @@ function fish_prompt
     if set -q VIRTUAL_ENV
         echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
     end
-    set pwd (sed -e 's/\/Users\/eddie/~/' (pwd | psub))
-    if test (echo $pwd | wc -c) -ge 40
+    set pwd (sed -e 's/\/Users\/eddie/~/' -e 's/\/home\/eddie/~/' (pwd | psub))
+    if test (echo $pwd | wc -c) -ge 70
         set pwd (prompt_pwd)
     end
     set_color $fish_color_cwd
+    if test (uname) = 'Linux'
+        echo -ne '\xce\xbb '
+    end
+    if test (uname) = 'Darwin'
+        echo -ne '\xef\xa3\xbf '
+    end
     echo -n $pwd
     set_color normal
     set branch (git branch ^/dev/null | grep \* | sed 's/* //')
